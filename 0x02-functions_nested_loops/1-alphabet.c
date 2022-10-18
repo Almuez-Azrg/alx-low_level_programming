b64372e787f5f5a890fd7e448e875ce9f1a14175
Almuez_Abd_Allah <114432495+Almuez-Azrg@users.noreply.github.com>
2022-10-18 22:37:01 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
-{
+	{
 	char ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-}
+	}
