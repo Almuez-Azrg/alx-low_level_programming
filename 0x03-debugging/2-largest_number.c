0902e3abeac388c4f510df9e69d8deefe40a7cb7
Almuez-Azrg <elsoltan-ezo@hotmail.com>
2022-10-20 05:22:00 -0700

    update code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,24 +11,22 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
-
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-
+int largest;
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 return (largest);
 }
