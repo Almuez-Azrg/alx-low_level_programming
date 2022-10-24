0b738d5786ce47c783efc94717ede6785d91ab8b
Almuez-Azrg <alsoltan-ezo@hotmail.com>
2022-10-24 14:46:07 -0700

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,10 +7,11 @@
  */
 void _puts(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
