aaa455cc982da5805a264f80b92ebc20a31e3702
Almuez_Abd_Allah <114432495+Almuez-Azrg@users.noreply.github.com>
2022-10-24 09:59:48 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* main-check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.
+ */
+
+void reset_to_98(int *n)
 {
-int n;
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+	*n = 98;
 }
