300fbb42da9fe957a2185a3a900d280fa591dece
Almuez_Abd_Allah <114432495+Almuez-Azrg@users.noreply.github.com>
2022-10-31 22:15:29 +0200

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,18 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: holberton.h
- * Auth: Firdaus
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -22,3 +13,4 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif
+Footer
