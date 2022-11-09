1d2b9adaa3b2296cd0f449cde0fda9ebe5fc38a3
Almuez-Azrg <elsoltan-ezo@hotmail.com>
2022-11-09 06:37:59 -0800

    update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>                                                                                                              #include <string.h>
-
+#include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
